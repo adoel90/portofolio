d4f00fa7d95f4db7046b6ac345db9d09182f2c5d
adoel90 <adoel.cs@gmail.com>
2018-08-27 01:47:31 +0700

    build 01.47

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// import logo from './logo.svg';
+// import './App.css';
+import LandingPage from './pages/LandingPage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ParallaxBackground from './components/ParallaxBackground';
+import ScrollTop from './components/ScrollTop';
+import ThemeSwitcher from './components/ThemeSwitcher';
+
+import Browser from './components/Browser';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Doel Portofolio</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div className="dev-landing-page">
+        <ThemeSwitcher>
+          <Browser except firefox>
+            <ParallaxBackground />
+          </Browser>
+          <LandingPage />
+          <AboutPage />
+          <PortfolioPage />
+          <ScrollTop />
+          <Footer />
+        </ThemeSwitcher>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './libs/fontawesome/fontawesome-all.min';
+import './styles/base.scss';
+// import './styles';  
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
